ed0145c91d11bdd34d2dff5bf8db2e7453bc34aa
mnmanjusha <106671871+mnmanjusha@users.noreply.github.com>
2023-08-18 16:39:52 +0530

    Update B.java
    Second Version

--- B.java
+++ B.java
@@ -2,6 +2,7 @@ class B{
 public static void main(String args[]){
    try{
       new B().go();
+      System.out.println("Modified Line");
   }
    catch(Exception e){ System.out.println("Exception"); }
 
